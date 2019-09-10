53cc082eed103533d1ce17d8f1dd90c648935116
william <will.dowling@boombah.com>
2019-09-10 13:29:53 -0500

    added sizes

--- 1_Configure.jsx
+++ 1_Configure.jsx
@@ -47,7 +47,13 @@ function buildConfig()
 							"2XL",
 							"3XL",
 							"4XL",
-							"5XL"
+							"5XL",
+							"XS-S",
+							"S-M",
+							"M-L",
+							"L-XL",
+							"XL-2XL",
+							"Giant Freaking Hat"
 						];
 
 	var pantsSizing = 
